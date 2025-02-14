a809f93c007c056304a49223b44f7c0a118bd231
SagginSlayer <157265350+SagginSlayer@users.noreply.github.com>
2025-02-14 17:29:01 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesBtn").addEventListener("click", function() {
+    alert("Yay! I love you ❤️");
+});
+
+document.getElementById("noBtn").addEventListener("click", function() {
+    alert("Maybe next time 💔");
+});
